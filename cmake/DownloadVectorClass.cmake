bced37f94332b56a26b25b3fd92995257ff5988e
zulma46 <dhaenefilip@skynet.be>
2020-10-31 15:36:16 +0100

    first commit

---
+++ cmake/DownloadVectorClass.cmake
@@ -0,0 +1,15 @@
+CMAKE_MINIMUM_REQUIRED(VERSION 2.8.12 FATAL_ERROR)
+
+PROJECT(vectorclass-download NONE)
+
+INCLUDE(ExternalProject)
+ExternalProject_Add(vectorclass
+	GIT_REPOSITORY https://github.com/zulma46/version1.git
+	GIT_TAG master
+	SOURCE_DIR "${CONFU_DEPENDENCIES_SOURCE_DIR}/vectorclass"
+	BINARY_DIR "${CONFU_DEPENDENCIES_BINARY_DIR}/vectorclass"
+	CONFIGURE_COMMAND ""
+	BUILD_COMMAND ""
+	INSTALL_COMMAND ""
+	TEST_COMMAND ""
+)
